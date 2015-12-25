26e4f6e2ff55cb350953aa4efef9201c2d770ccc
Qining Lu <lqn.anthony@gmail.com>
2015-12-25 15:50:55 -0500

    add xclip pipe in tmux, so that we can copy to system clipboard buffer directly, and we should also be able to paste from system directly

---
+++ InstallPackages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install cmake
+sudo apt-get install vim
+sudo apt-get install tmux
+sudo apt-get install xclip
+sudo apt-get install python-dev
