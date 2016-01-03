dc365721de78314c8f6abb1ee8116d74b22c98a5
Qining <lqn.anthony@gmail.com>
2016-01-03 03:31:08 -0500

    add ipython

--- InstallPackages.sh
+++ InstallPackages.sh
@@ -7,6 +7,7 @@ sudo apt-get install vim
 sudo apt-get install tmux
 sudo apt-get install xclip
 sudo apt-get install python-dev
+sudo apt-get install ipython
 sudo apt-get install cgdb
 sudo apt-get install meld
 sudo apt-get install clang-format-3.6
